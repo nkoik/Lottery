5839593ea8cfd9d1a2145551a0a26e3e07fb39cc
nkoik <24964750+nkoik@users.noreply.github.com>
2021-01-23 22:20:04 +0200

    Fix: Header button for logout added and style changes

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,21 +1,31 @@
 <template>
   <q-layout view="hHh LpR fFf">
 
-    <q-header class="bg-primary text-white" height-hint="98">
-      <q-toolbar>
+    <q-header class="bg-primary text-white row no-wrap" height-hint="98">
+      <q-toolbar class="col-8">
         <q-toolbar-title>
           <q-avatar>
             <img src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg">
           </q-avatar>
-          Title
+          Lottery
+        </q-toolbar-title>
+        <q-tabs align="left">
+          <q-route-tab :to="{ name: 'Home' }" label="Home" :disable="isDrawOpen" />
+          <q-route-tab :to="{ name: 'Draw' }" label="Live Draw" :disable="!isDrawOpen" />
+          <q-route-tab :to="{ name: 'History' }" label="History" :disable="isDrawOpen" />
+        </q-tabs>
+        <!-- <q-space /> -->
+      </q-toolbar>
+      <q-toolbar class="col-4">
+        <q-toolbar-title align="right">
+          <q-btn @click="handleLogoutUser">
+            <q-avatar>
+              <img src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg">
+            </q-avatar>
+            Logout
+          </q-btn>
         </q-toolbar-title>
       </q-toolbar>
-
-      <q-tabs align="left">
-        <q-route-tab to="/page1" label="Home" />
-        <q-route-tab to="/page2" label="Live Draw" />
-        <q-route-tab to="/page3" label="History" />
-      </q-tabs>
     </q-header>
 
     <q-page-container class="full-width row wrap justify-center items-center content-center">
@@ -26,10 +36,25 @@
 </template>
 
 <script>
+import { mapState, mapActions } from 'vuex'
 export default {
   component: 'MainLayout',
   data () {
     return {
+    }
+  },
+  computed: {
+    ...mapState({
+      isDrawOpen: state => state.draw.isDrawOpen
+    })
+  },
+  methods: {
+    ...mapActions({
+      logoutUser: 'authorization/logoutUser'
+    }),
+    handleLogoutUser () {
+      this.logoutUser()
+        .then(() => this.$router.replace({ name: 'Login' }))
     }
   }
 }
