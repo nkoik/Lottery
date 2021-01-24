fb2980f58a53bda5ba9326a59eae6a2e57556193
nkoik <24964750+nkoik@users.noreply.github.com>
2021-01-24 15:30:20 +0200

    Feature: Added styles and lottery table to Home page

---
+++ src/pages/Main/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-page class="home-container full-width flex flex-center bg-blue-2">
+    <lottery-ball-table
+      class="lottery-table"
+      v-model="selectedNumbers"
+    />
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  components: {
+    'lottery-ball-table': () => import(/* webpackPrefetch: true */ '../../components/BallTable.vue')
+  },
+  data () {
+    return {
+      selectedNumbers: []
+    }
+  }
+}
+</script>
