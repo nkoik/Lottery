15befa1f3d195fbd9d73947855a28b01ddcb1b5f
nkoik <24964750+nkoik@users.noreply.github.com>
2021-01-24 17:55:00 +0200

    Feature: BallTank component added

---
+++ src/components/BallTank.vue
@@ -0,0 +1,35 @@
+<template>
+    <q-card class="card-tank">
+      <div class="card-tank__title text-h6 bg-grey-4 q-pa-md">My BET</div>
+      <q-space/>
+      <q-separator />
+      <div class="row wrap items-center q-py-md text-center">
+        <lottery-ball
+          class="q-ma-sm"
+          @delete="handleDelete"
+          v-for="number in value"
+          :selected="value.includes(number)"
+          :key="number"
+          :label="number"
+          clearable
+        />
+      </div>
+    </q-card>
+</template>
+
+<script>
+export default {
+  name: 'LotteryBallTank',
+  props: {
+    value: {
+      type: [Array],
+      default: () => []
+    }
+  },
+  methods: {
+    handleDelete (number) {
+      this.$emit('input', this.value.filter(n => n !== number))
+    }
+  }
+}
+</script>
