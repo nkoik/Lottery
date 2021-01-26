546832a019bd15a5f91c892c8081428b36e3101f
nkoik <24964750+nkoik@users.noreply.github.com>
2021-01-26 12:14:06 +0200

    Feature: Added percentage and sum for history total amount

--- src/pages/Main/HistoryDrawList.vue
+++ src/pages/Main/HistoryDrawList.vue
@@ -1,42 +1,54 @@
 <template>
-  <q-list bordered class="history-list rounded-borders col-8 bg-white flex column flex-center" >
-    <q-item-label v-if="loadingData || paginatedDrawsList.length !== 0" header>All bets sorted by Date</q-item-label>
-    <template v-if="!loadingData && paginatedDrawsList.length !== 0">
-      <div class="full-width" v-for="draw in paginatedDrawsList" :key="draw.id">
-        <list-item
-          :ticket="{
-            id: draw.id,
-            prize: draw.prize,
-            won: draw.prize > 0
-          }"
-          @click.native="handleDrawDetails(draw.id)"
-          @delete="handleDeleteRow(draw.id)"
-        >
-          <lottery-ball
-            class="q-mx-xs"
-            v-for="ball in draw.numbers"
-            :label="ball"
-            size="sm"
-            :key="ball"
-            disabled
-            selected
-          />
-        </list-item>
-        <q-separator />
-      </div>
-    </template>
-    <div class="q-pa-lg" v-else-if="!loadingData">No bets</div>
-    <q-spinner size="lg" color="primary" v-else />
-    <q-pagination
-      v-if="!loadingData && paginatedDrawsList.length !== 0"
-      v-model="currentPage"
-      class="q-my-md"
-      color="primary"
-      :max="maxButtonsShown"
-      :max-pages="maxPerPage"
-      :boundary-numbers="true"
-    />
-  </q-list>
+  <div class="flex column col-8">
+    <div v-if="!loading && paginatedDrawsList.length !== 0" class="flex row justify-around q-mb-md history-list">
+      <q-card flat class="col-4 q-pa-md flex flex-center column">
+        <q-item-label class="text-h5 text-grey-8">Total Amount Won</q-item-label>
+        <q-card-section class="text-h6">{{ totalWon.sum + ' €' }}</q-card-section>
+      </q-card>
+      <q-card flat class="col-4 q-pa-md flex flex-center column">
+        <q-item-label class="text-h5 text-grey-8">Success Rate</q-item-label>
+        <q-card-section class="text-h6">{{ wonPercentage + '%' }}</q-card-section>
+      </q-card>
+    </div>
+    <q-list bordered class="history-list rounded-borders full-width bg-white flex column flex-center" >
+      <q-item-label v-if="loading || paginatedDrawsList.length !== 0" header>All bets sorted by Date</q-item-label>
+      <q-spinner v-if="loading"  size="lg" color="primary"/>
+      <template v-else-if="paginatedDrawsList.length !== 0">
+        <div class="full-width" v-for="draw in paginatedDrawsList" :key="draw.id">
+          <list-item
+            :ticket="{
+              id: draw.id,
+              prize: draw.prize,
+              won: draw.prize > 0
+            }"
+            @click.native="handleDrawDetails(draw.id)"
+            @delete="handleDeleteRow(draw.id)"
+          >
+            <lottery-ball
+              class="q-mx-xs"
+              v-for="ball in draw.numbers"
+              :label="ball"
+              size="sm"
+              :key="ball"
+              disabled
+              selected
+            />
+          </list-item>
+          <q-separator />
+        </div>
+      </template>
+      <div class="q-pa-lg" v-else>No bets</div>
+      <q-pagination
+        v-if="!loading && paginatedDrawsList.length !== 0"
+        v-model="currentPage"
+        class="q-my-md"
+        color="primary"
+        :max="maxButtonsShown"
+        :max-pages="maxPerPage"
+        :boundary-numbers="true"
+      />
+    </q-list>
+  </div>
 </template>
 
 <script>
@@ -50,7 +62,7 @@ export default {
   },
   data () {
     return {
-      loadingData: false,
+      loading: true,
       currentPage: 1
     }
   },
@@ -63,6 +75,17 @@ export default {
     },
     maxButtonsShown () {
       return Math.ceil(this.oldDraws.length / this.maxPerPage)
+    },
+    totalWon () {
+      return this.oldDraws.reduce((acc, val) => {
+        return {
+          sum: acc.sum + val.prize,
+          successCount: val.prize > 0 ? ++acc.successCount : acc.successCount
+        }
+      }, { sum: 0, successCount: 0 })
+    },
+    wonPercentage () {
+      return Math.round(this.totalWon.successCount / this.oldDraws.length * 100)
     }
   },
   methods: {
@@ -71,12 +94,13 @@ export default {
       deleteDraw: 'history/deleteDraw'
     }),
     handleDrawDetails (id) {
-      console.log(id)
       this.$router.push({ name: 'HistoryDrawDetails', params: { id } })
     },
     handleDeleteRow (id) {
+      this.loading = true
       this.deleteDraw(id)
         .then(() => {
+          this.loading = false
           this.$q.notify({
             color: 'green-4',
             textColor: 'white',
@@ -84,10 +108,22 @@ export default {
             message: 'Draw deleted from history board'
           })
         })
+        // eslint-disable-next-line
+        .catch((err) => {
+          this.loading = false
+          this.$q.notify({
+            color: 'red-4',
+            textColor: 'white',
+            message: 'Try again'
+          })
+        })
     }
   },
   mounted () {
     this.getDraws()
+      .finally(() => {
+        this.loading = false
+      })
   }
 }
 </script>
